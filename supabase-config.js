644e1640bf919592c28509601c82375184d97e79
ulakashasito-sys <ulakashasito@gmail.com>
2026-06-12 15:23:08 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+window.ULAKASHA_SUPABASE_URL = "https://hjlcboehxcubgewyhcmo.supabase.co";
+window.ULAKASHA_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbGNib2VoeGN1Ymdld3loY21vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNjA0NTMsImV4cCI6MjA5NjgzNjQ1M30.Egf-nrbM3h9-yvn1dHsGUg0RXFK55O3C8gSuFmc-A0g";
+
+window.ULAKASHA_NEWSLETTER_TABLE = "newsletter_subscribers";
+window.ULAKASHA_PRODUCTS_TABLE = "products";
+window.ULAKASHA_PRODUCT_IMAGES_BUCKET = "product-images";
+
+// Optional: paste a Make webhook URL here to send an email notification
+// when someone subscribes to the newsletter.
+window.ULAKASHA_MAKE_NEWSLETTER_WEBHOOK_URL = "https://hook.eu1.make.com/6qfy52lj85dj6r8b2g0r03elk9gk1dqq";
